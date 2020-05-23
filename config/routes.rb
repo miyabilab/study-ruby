6716bd6dedf28a876869a0c850abe3aac7a4acf3
miyabilab <miyabi@miyabilab.net>
2020-05-23 19:10:25 +0900

    section5-57

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'boards', to: 'boards#index'
-  get 'boards/new', to: 'boards#new'
-  get 'boards/:id', to: 'boards#show'
-  post 'boards', to: 'boards#create'
+  resources :boards, only: [:index, :new, :create, :show, :edit, :update]
 end
